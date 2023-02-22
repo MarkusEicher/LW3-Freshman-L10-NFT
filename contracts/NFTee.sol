b3dd8fcba64d4a8964ce3209c60cc24bd1e75310
MarkusEicher <markuseicher@outlook.com>
2023-02-22 06:27:41 +0100

    V0.1.2 - Bugfix missing import statement

--- contracts/NFTee.sol
+++ contracts/NFTee.sol
@@ -1,3 +1,11 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity 0.8.17;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract NFTee is ERC721 {
+    constructor() ERC721("LearnWeb3's NFT", "LEARN-NFT") {
+        _mint(msg.msg.sender, 1);
+        _mint(msg.msg.sender, 2);
+    }
+}
