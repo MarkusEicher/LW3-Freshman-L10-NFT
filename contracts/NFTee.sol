5d97a84d703a890394494a616492b88887b41b91
MarkusEicher <markuseicher@outlook.com>
2023-02-22 09:57:14 +0100

    v0.1.4 - Compiled contract

--- contracts/NFTee.sol
+++ contracts/NFTee.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.0;
+pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract NFTee is ERC721 {
     constructor() ERC721("LearnWeb3's NFT", "LEARN-NFT") {
-        _mint(msg.msg.sender, 1);
-        _mint(msg.msg.sender, 2);
+        _mint(msg.sender, 1);
+        _mint(msg.sender, 2);
     }
 }
